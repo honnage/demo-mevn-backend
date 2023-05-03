4d928b50d7ff68bedf889435fdac5188ed80ba77
honnage <honnage.x@gmail.com>
2023-05-03 12:46:41 +0700

    install dotenv and set path

--- server.js
+++ server.js
@@ -5,18 +5,22 @@
 import express  from 'express'
 import morgan  from 'morgan'
 import cors  from 'cors'
+import path from 'path'
+
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = 3066
 
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.set('port', process.env.PORT || port )
 
 app.listen(app.get('port'), () => {
-    console.log(`Server runnning on port ${port}`)
+    console.log(`Server runnning on port ${app.get('port')}`)
 })
