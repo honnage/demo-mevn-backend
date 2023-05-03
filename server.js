a8f515768d2a5e045a9f06eab61c22b5f44b9804
honnage <honnage.x@gmail.com>
2023-05-03 12:23:21 +0700

    install moran, cors

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.set('port', process.env.PORT || port )
 
-app.listen(app.get('prot'), () => {
+app.listen(app.get('port'), () => {
     console.log(`Server runnning on port ${port}`)
 })
