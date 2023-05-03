99c2c7bb9ded22be621cdf064201dab7af9385b8
honnage <honnage.x@gmail.com>
2023-05-03 13:36:57 +0700

    install mongoose and connec mongodb

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ import express  from 'express'
 import morgan  from 'morgan'
 import cors  from 'cors'
 import path from 'path'
-
+import mongoose from 'mongoose'
 require('dotenv').config()
+
+var url = "mongodb://localhost:27017/dbdemo"
+mongoose.Promise = global.Promise
+mongoose.connect(url, {
+    // useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Database connected!!'))
+.catch(err => console.error('Error connecting to database:', err))
+
 
 const app = express()
 const port = 3066
