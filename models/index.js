6287eee9b1aa46c91d96bf8224555d60ba2b5697
honnage <hon_da-1998@hotmail.com>
2023-05-22 15:22:01 +0700

    update api

--- models/index.js
+++ models/index.js
@@ -2,11 +2,14 @@ import Categoria from './categoria';
 import Articulo from './';
 import Usuario from './usuario';
 import Persona from './persona';
-
+import Ingreso from './ingreso';
+import Venta from './venta';
 
 export default {
     Categoria,
     Articulo,
     Usuario,
     Persona,
+    Ingreso,
+    Venta
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ import categoriaRouter from './categoria';
 import articuloRouter from './articulo';
 import usuarioRouter from './usuario';
 import personaRouter from './persona';
-
+import ingresoRouter from './ingreso';
+import ventaRouter from './venta';
 
 const router=routerx();
 
@@ -11,5 +12,6 @@ router.use('/categoria',categoriaRouter);
 router.use('/articulo',articuloRouter);
 router.use('/usuario',usuarioRouter);
 router.use('/persona',personaRouter);
-
+router.use('/ingreso',ingresoRouter);
+router.use('/venta',ventaRouter);
 export default router;
